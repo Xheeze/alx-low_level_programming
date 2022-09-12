d3e3e7153651b611b83c7d1553a07353e6e2d58f
Xheeze <ozo.sibiya@gmail.com>
2022-09-11 18:05:26 -0700

    another statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints set expression
+ *
+ * Return: always 0 for succesful run
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
