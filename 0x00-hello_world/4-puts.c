f454f2fd3b169cea16365d8626faed26221b13fc
Xheeze <ozo.sibiya@gmail.com>
2022-09-11 17:39:58 -0700

    Print statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Prints set expression
+ *
+ * Return: always 0 for succesful run
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle \n");
+    Return(0);
+}
