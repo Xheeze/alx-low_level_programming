8aa875a9815a57fbc7e3448fc725f1cb6eaa2730
Xheeze <ozo.sibiya@gmail.com>
2022-09-21 20:21:29 -0700

    Swap the UNmbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - rearranges integers
+*
+* @a: integer to be swapped
+* @b: integer to be swapped
+*/
+
+void int swap_int(*a, *b)
+{
+	*a=b
+	*b=a
+	a=98
+	b=42
+}
