0c4e9a5ddf96217a31ee280455fef4012991b17b
Ntokozo Sibiya <109238828+Xheeze@users.noreply.github.com>
2022-09-22 04:43:37 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 * reset_to_98 - takes a pointer to int "n"
 * & sets (*n) integer container with value 98
-* n - integer variable
+* @n: integer variable at pointer
 */
 
 	void reset_to_98(int *n)
