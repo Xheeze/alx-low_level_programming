1a2af3270ab8109b10892c803aaf0837ea6649a4
Xheeze <ozo.sibiya@gmail.com>
2022-09-22 10:12:47 -0700

    reset to n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
+* takes a pointer to int "n"
 *
-*takes a pointer to int "n"
-*
-*sets n to value 98
+* sets n to value 98
 * reset_to_98 - takes a pointer to int "n"
 * & sets (*n) integer container with value 98
 * @n: integer variable at pointer
