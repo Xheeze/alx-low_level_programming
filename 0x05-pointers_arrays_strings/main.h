9a8b3fc71c07d8be3887813e554d8d1c9c920d0b
Xheeze <ozo.sibiya@gmail.com>
2022-09-22 20:28:44 -0700

    every 2nd

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,14 +14,13 @@ int _strlen(char *s);
 */
 
 int _putchar(char c);
-/*void print_rev(char *s);*/
+void print_rev(char *s);
 void _puts(char *str);
 
-/*void rev_string(char *s);*/
-/*void puts2(char *str);*/
-/*void puts_half(char *str);*/
-
-/*void print_array(int *a, int n);*/
-/*char *_strcpy(char *dest, char *src;*/
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src;
 
 #endif
