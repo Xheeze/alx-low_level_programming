9a77473a2700ea1996e9b5bef5d9bea3b77b8ab6
Xheeze <ozo.sibiya@gmail.com>
2022-09-22 19:25:43 -0700

    copy neh

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,16 +1,24 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncpy - copies a string
- * @dest: copy destination
- * @src: string to be copied
- * @n: the maximum number of bytes copied from src
- *
- * Return: the pointer to the resulting string dest
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of digit to copy
+ * Return: value of dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
+}
