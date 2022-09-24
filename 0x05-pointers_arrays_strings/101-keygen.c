63dac554e272cad9991a928a1507f6a5b4bb2a94
Xheeze <ozo.sibiya@gmail.com>
2022-09-23 20:33:52 -0700

    randomo

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
+ * main - generates random valid passwords for the program 101-crackme.
+ *
+<<<<<<< HEAD
+ * Use man srand, rand, time, gdb and objdump to help.
+ *
+=======
+>>>>>>> 8f415b2b0a605b8ce356a0502fa8fb0d07dd516f
+ * Return: 0.
  */
+
 int main(void)
 {
-int num;
+	int pass;
+	char ch;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-printf("Tada! Congrats \n");
-return (0);
+	srand(time(NULL));
+	while (pass <= 2645)
+	{
+		ch = rand() % 128;
+		pass += ch;
+		putchar(ch);
+	}
+	putchar(2772 - pass);
+	printf("\n");
+
+	return (0);
 }
